524170787c654bcfdb90c46e4513eaa6982222a5
OishiSharmeen04 <ssultana324@gmail.com>
2025-10-09 17:51:46 +0600

    card

--- src/pages/AllApp/AllApp.jsx
+++ src/pages/AllApp/AllApp.jsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React from "react";
+import { IoMdDownload } from "react-icons/io";
+import { FaStar } from "react-icons/fa";
 
-const AllApp = (singleApp) => {
-    
-const {image} = singleApp;
+const AllApp = ({ singleApp }) => {
+  const { companyName, image, downloads, ratingAvg } = singleApp;
 
-    return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src={image}
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+  const formatDownloads = (num) => {
+    if (num >= 1000000) {
+      return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
+    } else if (num >= 1000) {
+      return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K";
+    } else {
+      return num.toString();
+    }
+  };
+
+  return (
+    <div
+      className="
+        card bg-base-100 shadow-sm 
+        hover:shadow-lg 
+        transform transition-transform duration-300 
+        hover:-translate-y-2
+      "
+    >
+      <figure className="p-4">
+        <img
+          src={image}
+          alt={companyName}
+          className="rounded-xl object-cover"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-lg font-semibold">{companyName}</h2>
+
+        <div className="card-actions justify-between mt-2">
+          <div className="badge badge-outline"><IoMdDownload />{formatDownloads(downloads)}</div>
+          <div className="badge badge-outline"><FaStar />{ratingAvg}</div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default AllApp;
